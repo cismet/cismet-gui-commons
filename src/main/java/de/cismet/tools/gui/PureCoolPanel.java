93359f9726053cc9f65a9d75df9554619319db0a
Stefan Richter <stefan.richter@cismet.de>
2009-09-18 11:32:04 +0000

    issue[0001066]

--- src/main/java/de/cismet/tools/gui/PureCoolPanel.java
+++ src/main/java/de/cismet/tools/gui/PureCoolPanel.java
@@ -10,6 +10,7 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Stroke;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.image.BufferedImage;
@@ -38,23 +39,23 @@ public class PureCoolPanel extends JPanel implements ComponentListener {
     // Lumbermill Logger initialisieren
     private final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass());
     @InjectedResource()
-    public int offset,  offsetRight,  offsetTop,  offsetBetween,  arcSize,  shadowLength,  blurFactor;
+    public int offset, offsetRight, offsetTop, offsetBetween, arcSize, shadowLength, blurFactor;
     @InjectedResource()
-    public float borderWidth,  shadowIntensity,  titlePanelOpacity,  titleLinesOpacity,  interPanelOpacity,  interLinesOpacity,  blurredMapOpacity,  cutOutMapOpacity,  glossyOpacity;
+    public float borderWidth, shadowIntensity, titlePanelOpacity, titleLinesOpacity, interPanelOpacity, interLinesOpacity, blurredMapOpacity, cutOutMapOpacity, glossyOpacity;
     @InjectedResource()
-    public Color shadowColor,  colorBorder,  colorMapBorder,  colorTitle,  colorInter,  colorDarkLine,  colorBrightLine,  colorGlossy,  gradientColorTop,  gradientColorBottom;
+    public Color shadowColor, colorBorder, colorMapBorder, colorTitle, colorInter, colorDarkLine, colorBrightLine, colorGlossy, gradientColorTop, gradientColorBottom;
     private static final int IMAGE_TYPE = BufferedImage.TYPE_4BYTE_ABGR;
-    private boolean noTitlePanel,  mustBlur;
+    private boolean noTitlePanel, mustBlur;
     private double geoBuffer;
-    private int lastX,  lastWidth,  panelWidth;
+    private int lastX, lastWidth, panelWidth;
     private ImageIcon icons;
     private Image map;
-    private BufferedImage cacheImage,  gradientImage,  blurredMap,  cachedBlurredMap,  orgMap;
+    private BufferedImage cacheImage, gradientImage, blurredMap, cachedBlurredMap, orgMap;
     private JPanel spinner;
-    private JComponent panTitle,  panMap,  panInter,  panContent;
+    private JComponent panTitle, panMap, panInter, panContent;
     private Rectangle mapBounds;
-
-
+    private final Composite composite;
+    private final ShadowRenderer shadowRenderer;
 
     /**
      * Kontruktor des CoolPanels mit einer Puffergroesse. Diese wird dazu verwendet,
@@ -77,9 +78,10 @@ public class PureCoolPanel extends JPanel implements ComponentListener {
         // Ressourcen hierarchisch rekursiv nach oben einfuegen
         ResourceInjector.get("purecoolpanel.style").inject(true, getInstance());
 //
-        gradientColorTop=javax.swing.UIManager.getDefaults().getColor("Button.shadow");
-        gradientColorBottom=javax.swing.UIManager.getDefaults().getColor("Button.background");
-
+        gradientColorTop = javax.swing.UIManager.getDefaults().getColor("Button.shadow");
+        gradientColorBottom = javax.swing.UIManager.getDefaults().getColor("Button.background");
+        composite = AlphaComposite.SrcAtop.derive(titleLinesOpacity);
+        shadowRenderer = new ShadowRenderer(shadowLength, shadowIntensity, shadowColor);
         initImagesAndMore();
         this.addComponentListener(this);
 
@@ -112,33 +114,36 @@ public class PureCoolPanel extends JPanel implements ComponentListener {
      * @param icon das zu zeichnende Icon.
      */
     public void setImageRechtsOben(ImageIcon icon) {
-        try {
+        if (icon != null) {
+            try {
 //            ReflectionRenderer renderer2 = new ReflectionRenderer(0.5f,0.4f,true);
-            ShadowRenderer renderer = new ShadowRenderer(3, 0.5f, Color.BLACK);
+                ShadowRenderer renderer = new ShadowRenderer(3, 0.5f, Color.BLACK);
 
-            BufferedImage temp = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
-                    IMAGE_TYPE);
-            Graphics tg = temp.createGraphics();
-            tg.drawImage(icon.getImage(), 0, 0, null);
-            tg.dispose();
+                BufferedImage temp = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
+                        IMAGE_TYPE);
+                Graphics tg = temp.createGraphics();
+                tg.drawImage(icon.getImage(), 0, 0, null);
+                tg.dispose();
 
-            BufferedImage shadow = renderer.createShadow(temp);
+                BufferedImage shadow = renderer.createShadow(temp);
 
-            BufferedImage result = new BufferedImage(icon.getIconWidth() + 2 * shadowLength,
-                    icon.getIconHeight() + 2 * shadowLength, IMAGE_TYPE);
-            Graphics rg = result.createGraphics();
-            rg.drawImage(shadow, 0, 0, null);
-            rg.drawImage(temp, 0, 0, null);
-            rg.dispose();
-            shadow.flush();
+                BufferedImage result = new BufferedImage(icon.getIconWidth() + 2 * shadowLength,
+                        icon.getIconHeight() + 2 * shadowLength, IMAGE_TYPE);
+                Graphics rg = result.createGraphics();
+                rg.drawImage(shadow, 0, 0, null);
+                rg.drawImage(temp, 0, 0, null);
+                rg.dispose();
+                shadow.flush();
 
 //            BufferedImage ref = renderer.appendReflection(tmp);
-            ImageIcon icons = new ImageIcon(result);
-            this.icons = icons;
-        } catch (Exception e) {
-            this.icons = null;
+                ImageIcon icons = new ImageIcon(result);
+                this.icons = icons;
+            } catch (Exception e) {
+                this.icons = null;
+            }
         }
     }
+    private static final Stroke STROKE = new BasicStroke(1.0f);
 
     /**
      * ueberschreibt die Standard-Zeichenmethode eines JPanels. Zeichnet die "coolen"
@@ -149,82 +154,82 @@ public class PureCoolPanel extends JPanel implements ComponentListener {
     protected void paintComponent(Graphics g) {
         //        log.info("CoolPanel: paintComponent()");
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
+        final Graphics2D g2d = (Graphics2D) g;
 
         if (cacheImage == null) {
             // Image zum Zeichnen erstellen von dem wird spaeter der Schlagschatten erstellt wird
-            BufferedImage box = new BufferedImage(getWidth() - offset, getHeight() - offset, IMAGE_TYPE);
+            final BufferedImage box = new BufferedImage(getWidth() - offset, getHeight() - offset, IMAGE_TYPE);
 
             // Graphics-Objekt der Box erzeugen
-            Graphics2D bg = box.createGraphics();
+            final Graphics2D boxGraphics = box.createGraphics();
 
             // Standard-Zeichenmodus speichern
-            Composite orig = bg.getComposite();
+            final Composite originalComposite = boxGraphics.getComposite();
 
             if (gradientImage == null || gradientImage.getHeight() != box.getHeight()) {
                 gradientImage = GraphicsUtilities.createCompatibleImage(1, box.getHeight());
-                Graphics2D grad2d = gradientImage.createGraphics();
-                grad2d.setPaint(new GradientPaint(0, 0, gradientColorTop, 0, box.getHeight(), gradientColorBottom));
-                grad2d.fillRect(0, 0, 1, box.getHeight());
+                final Graphics2D gradientGraphics2d = gradientImage.createGraphics();
+                gradientGraphics2d.setPaint(new GradientPaint(0, 0, gradientColorTop, 0, box.getHeight(), gradientColorBottom));
+                gradientGraphics2d.fillRect(0, 0, 1, box.getHeight());
             }
 
             // RoundedRectangle zeichnen und mit Gradient fuellen
-            bg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            bg.setColor(Color.BLACK);
-            bg.fillRoundRect(offset, 0, box.getWidth() - offset, box.getHeight(), arcSize, arcSize);
-            bg.setComposite(AlphaComposite.SrcAtop);
-            bg.drawImage(gradientImage, 0, 0, box.getWidth(), box.getHeight(), null);
-            bg.setComposite(orig);
-            bg.setStroke(new BasicStroke(1.0f));
+//            boxGraphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            boxGraphics.setColor(Color.BLACK);
+            boxGraphics.fillRoundRect(offset, 0, box.getWidth() - offset, box.getHeight(), arcSize, arcSize);
+            boxGraphics.setComposite(AlphaComposite.SrcAtop);
+            boxGraphics.drawImage(gradientImage, 0, 0, box.getWidth(), box.getHeight(), null);
+            boxGraphics.setComposite(originalComposite);
+            boxGraphics.setStroke(STROKE);
 
             // Falls TitlePanel existiert, speziell zeichnen
             if (getPanTitle() != null) {
                 noTitlePanel = false;
                 Rectangle bounds = getPanTitle().getBounds();
-                bg.setComposite(AlphaComposite.SrcAtop.derive(titlePanelOpacity));
-                bg.setColor(colorTitle);
-                bg.fillRect(0, bounds.y, bounds.width + offset, bounds.height);
-                bg.setComposite(AlphaComposite.SrcAtop.derive(titleLinesOpacity));
-                bg.setColor(colorDarkLine);
-                bg.drawLine(offset, bounds.height - 1, bounds.width + 3 * offset, bounds.height - 1);
-                bg.setColor(colorBrightLine);
-                bg.drawLine(offset, bounds.height, bounds.width + 3 * offset, bounds.height);
+                boxGraphics.setComposite(AlphaComposite.SrcAtop.derive(titlePanelOpacity));
+                boxGraphics.setColor(colorTitle);
+                boxGraphics.fillRect(0, bounds.y, bounds.width + offset, bounds.height);
+                boxGraphics.setComposite(composite);
+                boxGraphics.setColor(colorDarkLine);
+                boxGraphics.drawLine(offset, bounds.height - 1, bounds.width + 3 * offset, bounds.height - 1);
+                boxGraphics.setColor(colorBrightLine);
+                boxGraphics.drawLine(offset, bounds.height, bounds.width + 3 * offset, bounds.height);
             }
 
             // Falls InteractionPanel existiert, speziell zeichnen
             if (getPanInter() != null) {
-                Rectangle bounds = getPanInter().getBounds();
-                bg.setComposite(AlphaComposite.SrcAtop.derive(interPanelOpacity));
+                final Rectangle bounds = getPanInter().getBounds();
+                boxGraphics.setComposite(AlphaComposite.SrcAtop.derive(interPanelOpacity));
 //            bg.setColor(colInter);
-                bg.setPaint(new GradientPaint(0, bounds.y, new Color(0, 0, 0, 160), 0, bounds.y + bounds.height, Color.BLACK));
-                bg.fillRect(0, bounds.y, bounds.width + offset, bounds.height);
-                bg.setComposite(AlphaComposite.SrcAtop.derive(interLinesOpacity));
-                bg.setColor(colorDarkLine);
-                bg.drawLine(offset, bounds.y - 1, bounds.width + 3 * offset, bounds.y - 1);
-                bg.setColor(colorBrightLine);
-                bg.drawLine(offset, bounds.y, bounds.width + 3 * offset, bounds.y);
+                boxGraphics.setPaint(new GradientPaint(0, bounds.y, new Color(0, 0, 0, 160), 0, bounds.y + bounds.height, Color.BLACK));
+                boxGraphics.fillRect(0, bounds.y, bounds.width + offset, bounds.height);
+                boxGraphics.setComposite(AlphaComposite.SrcAtop.derive(interLinesOpacity));
+                boxGraphics.setColor(colorDarkLine);
+                boxGraphics.drawLine(offset, bounds.y - 1, bounds.width + 3 * offset, bounds.y - 1);
+                boxGraphics.setColor(colorBrightLine);
+                boxGraphics.drawLine(offset, bounds.y, bounds.width + 3 * offset, bounds.y);
             }
 
             // Rahmen des RoundRectangel in der Box nachzeichnen
-            bg.setComposite(AlphaComposite.SrcOver.derive(0.7f));
-            bg.setColor(colorBorder);
-            bg.drawRoundRect(offset, 0, box.getWidth() - (offset + 1), box.getHeight() - 1, arcSize, arcSize);
+            boxGraphics.setComposite(AlphaComposite.SrcOver.derive(0.7f));
+            boxGraphics.setColor(colorBorder);
+            boxGraphics.drawRoundRect(offset, 0, box.getWidth() - (offset + 1), box.getHeight() - 1, arcSize, arcSize);
 
             // Weissen oberen Rand zeichnen
-            BufferedImage glossy = new BufferedImage(box.getWidth(), box.getHeight(), IMAGE_TYPE);
-            Graphics2D gg = glossy.createGraphics();
-            gg.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            gg.setStroke(new BasicStroke(1.0f));
+            final BufferedImage glossy = new BufferedImage(box.getWidth(), box.getHeight(), IMAGE_TYPE);
+            final Graphics2D glossyGraphics2D = glossy.createGraphics();           
+//            glossyGraphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            glossyGraphics2D.setStroke(STROKE);
 
             // Glossy-Effekt oben
-            gg.setColor(colorGlossy);
+            glossyGraphics2D.setColor(colorGlossy);
 //            if (noTitlePanel) {
-            gg.drawRoundRect(offset + 1, 1, box.getWidth() - (offset + 3), 2 * arcSize, arcSize, arcSize);
-            gg.setComposite(AlphaComposite.DstIn);
-            gg.setPaint(new GradientPaint(0, 0, new Color(255, 255, 255, 255), 0, arcSize / 2, new Color(255, 255, 255, 0)));
-            gg.fillRect(0, 0, box.getWidth(), arcSize);
-            gg.setPaint(new Color(255, 255, 255, 0));
-            gg.fillRect(0, arcSize / 2, box.getWidth(), 2 * arcSize);
+            glossyGraphics2D.drawRoundRect(offset + 1, 1, box.getWidth() - (offset + 3), 2 * arcSize, arcSize, arcSize);
+            glossyGraphics2D.setComposite(AlphaComposite.DstIn);
+            glossyGraphics2D.setPaint(new GradientPaint(0, 0, new Color(255, 255, 255, 255), 0, arcSize / 2, new Color(255, 255, 255, 0)));
+            glossyGraphics2D.fillRect(0, 0, box.getWidth(), arcSize);
+            glossyGraphics2D.setPaint(new Color(255, 255, 255, 0));
+            glossyGraphics2D.fillRect(0, arcSize / 2, box.getWidth(), 2 * arcSize);
 //            } else {
 //                gg.fillRoundRect(offset+2,2, box.getWidth()-(offset+4), getPanTitle().getHeight(),arcSize-2,arcSize-2);
 //                gg.setComposite(AlphaComposite.DstIn);
@@ -235,23 +240,22 @@ public class PureCoolPanel extends JPanel implements ComponentListener {
 //            }
 
             // Drop Shadow rendern
-            ShadowRenderer renderer = new ShadowRenderer(shadowLength, shadowIntensity, shadowColor);
-            BufferedImage shadow = renderer.createShadow(box);
+            final BufferedImage shadow = shadowRenderer.createShadow(box);
 
             // Fertige Box und ihren Schatten zeichnen.
             cacheImage = new BufferedImage(shadow.getWidth(), shadow.getHeight(), IMAGE_TYPE);
-            Graphics2D cg = cacheImage.createGraphics();
-            cg.drawImage(shadow, 0, 0, null);
-            cg.drawImage(box, 0, 0, null);
-            cg.setComposite(AlphaComposite.SrcOver.derive(glossyOpacity));
-            cg.drawImage(glossy, 0, 0, null);
-            cg.setComposite(orig);
+            final Graphics2D resultGraphics2D = cacheImage.createGraphics();
+            resultGraphics2D.drawImage(shadow, 0, 0, null);
+            resultGraphics2D.drawImage(box, 0, 0, null);
+            resultGraphics2D.setComposite(AlphaComposite.SrcOver.derive(glossyOpacity));
+            resultGraphics2D.drawImage(glossy, 0, 0, null);
+            resultGraphics2D.setComposite(originalComposite);
             if (this.icons != null) {
-                cg.drawImage(this.icons.getImage(), box.getWidth() - this.icons.getIconWidth() - offsetRight, offsetTop, null);
+                resultGraphics2D.drawImage(this.icons.getImage(), box.getWidth() - this.icons.getIconWidth() - offsetRight, offsetTop, null);
             }
-            bg.dispose();
-            gg.dispose();
-            cg.dispose();
+            boxGraphics.dispose();
+            glossyGraphics2D.dispose();
+            resultGraphics2D.dispose();
             box.flush();
             glossy.flush();
             shadow.flush();
@@ -261,8 +265,6 @@ public class PureCoolPanel extends JPanel implements ComponentListener {
         g2d.drawImage(cacheImage, 0, 0, null);
 
     }
-
-
 
     public JComponent getPanTitle() {
         return panTitle;

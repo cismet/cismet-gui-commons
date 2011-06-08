6fcbe9c516a6f3d51330c7116533baf14c45a612
Jens Weintraut <jens.weintraut@cismet.de>
2011-06-08 10:51:43 +0000

    [issue:2123] - added: download manager classes

---
+++ src/main/java/de/cismet/tools/gui/downloadmanager/DownloadListChangedListener.java
@@ -0,0 +1,28 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.tools.gui.downloadmanager;
+
+import java.util.EventListener;
+
+/**
+ * The interface for DownloadListChangedListeners.
+ *
+ * @author   jweintraut
+ * @version  $Revision$, $Date$
+ */
+public interface DownloadListChangedListener extends EventListener {
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * The method to be called by download manager when the download list has changed.
+     *
+     * @param  event  A DownloadListChangedEvent containing the download that has changed.
+     */
+    void downloadListChanged(DownloadListChangedEvent event);
+}
---
+++ src/main/java/de/cismet/tools/gui/downloadmanager/DownloadManager.java
@@ -0,0 +1,299 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.tools.gui.downloadmanager;
+
+import Sirius.navigator.resource.PropertyManager;
+
+import org.apache.log4j.Logger;
+
+import org.jdom.Element;
+
+import java.io.File;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.event.EventListenerList;
+
+import de.cismet.tools.configuration.Configurable;
+import de.cismet.tools.configuration.NoWriteError;
+
+/**
+ * The download manager manages all current downloads. New downloads are added to a collection, completed downloads are
+ * removed. Erraneous downloads remain in the collection. The download manager observes all download objects for state
+ * changed and informs the download manager panel via the DownloadListChangedListener interface.
+ *
+ * @author   jweintraut
+ * @version  $Revision$, $Date$
+ */
+public class DownloadManager implements Observer /*, Configurable*/ {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final Logger LOG = Logger.getLogger(DownloadManager.class);
+    private static DownloadManager instance = null;
+
+    private static final String XML_CONF_ROOT = "downloadManager";
+    private static final String XML_DESTINATION_DIRECTORY = "destinationDirectory";
+
+    //~ Instance fields --------------------------------------------------------
+
+    private boolean enabled;
+    private File destinationDirectory;
+    private LinkedList<Download> downloads = new LinkedList<Download>();
+    private EventListenerList listeners = new EventListenerList();
+    private int countDownloadsTotal = 0;
+    private int countDownloadsRunning = 0;
+    private int countDownloadsErraneous = 0;
+    private int countDownloadsCompleted = 0;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new DownloadManager object.
+     */
+    private DownloadManager() {
+        if (PropertyManager.getManager().getDownloadDestination() != null) {
+            destinationDirectory = PropertyManager.getManager().getDownloadDestination();
+            if (!destinationDirectory.isDirectory() || !destinationDirectory.canWrite()) {
+                LOG.error("The download manager can't use the directory '" + destinationDirectory.getAbsolutePath()
+                            + "'. The download manager will be disabled.");
+                enabled = false;
+            } else {
+                enabled = true;
+            }
+        }
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * It's a Singleton. There can only be one download manager.
+     *
+     * @return  The download manager.
+     */
+    public static DownloadManager instance() {
+        if (instance == null) {
+            instance = new DownloadManager();
+        }
+
+        return instance;
+    }
+
+    /**
+     * This method is used to add new downloads to the download list.
+     *
+     * @param  downloads  wfss A collection of downloads to add.
+     */
+    public void add(final Collection<Download> downloads) {
+        if ((downloads == null) || (downloads.size() <= 0)) {
+            return;
+        }
+
+        this.downloads.addAll(0, downloads);
+        countDownloadsTotal += downloads.size();
+
+        for (final Download download : downloads) {
+            download.addObserver(this);
+            download.startDownload();
+        }
+
+        notifyDownloadListChanged(new DownloadListChangedEvent(
+                this,
+                downloads,
+                DownloadListChangedEvent.Action.ADDED));
+        notifyDownloadListChanged(new DownloadListChangedEvent(
+                this,
+                downloads,
+                DownloadListChangedEvent.Action.CHANGED_COUNTERS));
+    }
+
+    /**
+     * Removes oboslete downloads. Only running downloads aren't obsolote.
+     */
+    public synchronized void removeObsoleteDownloads() {
+        final Collection<Download> downloadsRemoved = new LinkedList<Download>();
+
+        for (final Download download : downloads) {
+            if ((download.getStatus() == Download.COMPLETED) || (download.getStatus() == Download.ERROR)) {
+                downloadsRemoved.add(download);
+            }
+        }
+
+        if (downloadsRemoved.size() > 0) {
+            for (final Download download : downloadsRemoved) {
+                downloads.remove(download);
+                countDownloadsTotal--;
+
+                switch (download.getStatus()) {
+                    case Download.ERROR: {
+                        countDownloadsErraneous--;
+                        break;
+                    }
+                    case Download.COMPLETED: {
+                        countDownloadsCompleted--;
+                        break;
+                    }
+                }
+            }
+
+            notifyDownloadListChanged(new DownloadListChangedEvent(
+                    this,
+                    downloadsRemoved,
+                    DownloadListChangedEvent.Action.REMOVED));
+            notifyDownloadListChanged(new DownloadListChangedEvent(
+                    this,
+                    downloadsRemoved,
+                    DownloadListChangedEvent.Action.CHANGED_COUNTERS));
+        }
+    }
+
+    /**
+     * Returns the current download list.
+     *
+     * @return  The current download list.
+     */
+    public Collection<Download> getDownloads() {
+        return downloads;
+    }
+
+    /**
+     * Returns the count of erraneous downloads.
+     *
+     * @return  The count of erraneous downloads.
+     */
+    public int getCountDownloadsErraneous() {
+        return countDownloadsErraneous;
+    }
+
+    /**
+     * Returns the count of running downloads.
+     *
+     * @return  The count of running downloads.
+     */
+    public int getCountDownloadsRunning() {
+        return countDownloadsRunning;
+    }
+
+    /**
+     * Returns the count of completed downloads.
+     *
+     * @return  The count of completed downloads.
+     */
+    public int getCountDownloadsCompleted() {
+        return countDownloadsCompleted;
+    }
+
+    /**
+     * Returns the total count of downloads.
+     *
+     * @return  The total count of downloads.
+     */
+    public int getCountDownloadsTotal() {
+        return countDownloadsTotal;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public File getDestinationDirectory() {
+        return destinationDirectory;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public synchronized void update(final Observable o, final Object arg) {
+        if (!(o instanceof Download)) {
+            return;
+        }
+
+        final Download download = (Download)o;
+
+        switch (download.getStatus()) {
+            case Download.COMPLETED: {
+                countDownloadsRunning--;
+                countDownloadsCompleted++;
+                break;
+            }
+            case Download.ERROR: {
+                countDownloadsRunning--;
+                countDownloadsErraneous++;
+                break;
+            }
+            case Download.RUNNING: {
+                countDownloadsRunning++;
+                break;
+            }
+        }
+
+        notifyDownloadListChanged(new DownloadListChangedEvent(
+                this,
+                download,
+                DownloadListChangedEvent.Action.CHANGED_COUNTERS));
+    }
+
+    /**
+     * Adds a new DownloadListChangedListener.
+     *
+     * @param  listener  The listener to add.
+     */
+    public void addDownloadListChangedListener(final DownloadListChangedListener listener) {
+        listeners.add(DownloadListChangedListener.class, listener);
+    }
+
+    /**
+     * Removes a DownloadListChangedListener.
+     *
+     * @param  listener  The listener to remove.
+     */
+    public void removeDownloadListChangedListener(final DownloadListChangedListener listener) {
+        listeners.remove(DownloadListChangedListener.class, listener);
+    }
+
+    /**
+     * Notifies all current DownloadListChangedListeners.
+     *
+     * @param  event  The event to notify about.
+     */
+    protected synchronized void notifyDownloadListChanged(final DownloadListChangedEvent event) {
+        for (final DownloadListChangedListener listener : listeners.getListeners(DownloadListChangedListener.class)) {
+            listener.downloadListChanged(event);
+        }
+    }
+
+    /*@Override
+     * public void configure(Element parent) { }
+     *
+     * @Override public void masterConfigure(Element parent) { String calculatedDestinationDirectory =
+     * System.getProperty("user.home"); final Element downloadManager = parent.getChild(XML_CONF_ROOT); Element
+     * destinationDirectory = null;  if (downloadManager == null) {     LOG.warn("The download manager isn't configured.
+     * The download manager will use the home directory for downloads."); } else {     destinationDirectory =
+     * downloadManager.getChild(XML_DESTINATION_DIRECTORY);     if (destinationDirectory == null) {
+     * LOG.warn("The destination directory isn't configured. The download manager will use the home directory for
+     * downloads.");     } }  if(destinationDirectory != null) {     String configuredDestinationDirectory =
+     * destinationDirectory.getText();     if(configuredDestinationDirectory != null &&
+     * configuredDestinationDirectory.trim().length() > 0) {         calculatedDestinationDirectory =
+     * configuredDestinationDirectory;     } }  this.destinationDirectory = new File(calculatedDestinationDirectory);
+     * if(!this.destinationDirectory.isDirectory() || !this.destinationDirectory.canWrite()) {     LOG.error("The
+     * download manager can't use the directory '" + calculatedDestinationDirectory + "'. The download manager will be
+     * disabled.");     enabled = false; } else {     enabled = true; } }
+     *
+     * @Override public Element getConfiguration() throws NoWriteError { return null;}*/
+}

3cf4bd37f13682f10f422210a8210c31ea6f17a3
Thorsten Hell <thorsten.hell@cismet.de>
2011-09-15 15:13:27 +0000

    [issue:2244] Developer Options Panel

---
+++ src/main/java/de/cismet/lookupoptions/options/CismetDeveloperCategory.java
@@ -0,0 +1,51 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.lookupoptions.options;
+
+import org.openide.util.ImageUtilities;
+import org.openide.util.lookup.ServiceProvider;
+
+import java.awt.Image;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import de.cismet.lookupoptions.AbstractOptionsCategory;
+import de.cismet.lookupoptions.OptionsCategory;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   thorsten
+ * @version  $Revision$, $Date$
+ */
+@ServiceProvider(service = OptionsCategory.class)
+public class CismetDeveloperCategory extends AbstractOptionsCategory {
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public String getName() {
+        return org.openide.util.NbBundle.getMessage(CismetDeveloperCategory.class, "CismetDeveloperCategory.name"); // NOI18N
+    }
+
+    @Override
+    public Icon getIcon() {
+        final Image image = ImageUtilities.loadImage("de/cismet/lookupoptions/options/cismetlogo32.png"); // NOI18N
+        if (image != null) {
+            return new ImageIcon(image);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return 99;
+    }
+}

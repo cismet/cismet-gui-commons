10d3efbc938b71deddc0c41391e3ea49da6ea4fb
Thorsten Herter <thorsten.herter@cismet.de>
2012-05-29 16:05:42 +0000

    [issue:0002575] WebDav-Downloads mit dem DownloadManager

---
+++ src/main/java/de/cismet/tools/gui/downloadmanager/WebDavDownload.java
@@ -0,0 +1,166 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.tools.gui.downloadmanager;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import de.cismet.security.WebDavClient;
+
+import de.cismet.security.exceptions.AccessMethodIsNotSupportedException;
+import de.cismet.security.exceptions.MissingArgumentException;
+import de.cismet.security.exceptions.NoHandlerForURLException;
+import de.cismet.security.exceptions.RequestFailedException;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   therter
+ * @version  $Revision$, $Date$
+ */
+public class WebDavDownload extends AbstractDownload {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    private static final int MAX_BUFFER_SIZE = 1024;
+
+    //~ Instance fields --------------------------------------------------------
+
+    private WebDavClient client;
+    private String path;
+    private String filename;
+    private String extension;
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new WebDavDownload object.
+     *
+     * @param  client     DOCUMENT ME!
+     * @param  path       DOCUMENT ME!
+     * @param  directory  DOCUMENT ME!
+     * @param  title      DOCUMENT ME!
+     * @param  filename   DOCUMENT ME!
+     * @param  extension  DOCUMENT ME!
+     */
+    public WebDavDownload(final WebDavClient client,
+            final String path,
+            final String directory,
+            final String title,
+            final String filename,
+            final String extension) {
+        this.client = client;
+        this.path = path;
+        this.directory = directory;
+        this.title = title;
+        this.filename = filename;
+        this.extension = extension;
+
+        if (DownloadManager.instance().isEnabled()) {
+            determineDestinationFile(filename, extension);
+            status = State.WAITING;
+        } else {
+            status = State.COMPLETED_WITH_ERROR;
+            caughtException = new Exception("DownloadManager is disabled. Cancelling download.");
+        }
+    }
+
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public void run() {
+        if (status != State.WAITING) {
+            return;
+        }
+
+        status = State.RUNNING;
+
+        FileOutputStream out = null;
+        InputStream resp = null;
+
+        stateChanged();
+
+        try {
+            resp = client.getInputStream(path);
+
+            out = new FileOutputStream(fileToSaveTo);
+            boolean downloading = true;
+            while (downloading) {
+                // Size buffer according to how much of the file is left to download.
+                final byte[] buffer;
+                buffer = new byte[MAX_BUFFER_SIZE];
+
+                // Read from server into buffer.
+                final int read = resp.read(buffer);
+                if (read == -1) {
+                    downloading = false;
+                } else {
+                    // Write buffer to file.
+                    out.write(buffer, 0, read);
+                }
+            }
+        } catch (Exception ex) {
+            error(ex);
+        } finally {
+            // Close file.
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e) {
+                    log.warn("Exception occured while closing file.", e);
+                }
+            }
+
+            // Close connection to server.
+            if (resp != null) {
+                try {
+                    resp.close();
+                } catch (Exception e) {
+                    log.warn("Exception occured while closing response stream.", e);
+                }
+            }
+        }
+
+        if (status == State.RUNNING) {
+            status = State.COMPLETED;
+            stateChanged();
+        }
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (!(obj instanceof WebDavDownload)) {
+            return false;
+        }
+
+        final WebDavDownload other = (WebDavDownload)obj;
+
+        boolean result = true;
+
+        if ((this.path != other.path) && ((this.path == null) || !this.path.equals(other.path))) {
+            result &= false;
+        }
+
+        if ((this.fileToSaveTo == null) ? (other.fileToSaveTo != null)
+                                        : (!this.fileToSaveTo.equals(other.fileToSaveTo))) {
+            result &= false;
+        }
+
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+
+        hash = (43 * hash) + ((this.path != null) ? this.path.hashCode() : 0);
+        hash = (43 * hash) + ((this.fileToSaveTo != null) ? this.fileToSaveTo.hashCode() : 0);
+
+        return hash;
+    }
+}

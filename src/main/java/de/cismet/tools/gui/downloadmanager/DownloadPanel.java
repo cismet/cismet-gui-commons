ede8b76ca0d21ad95c7f96c73f7701e419a1be37
Daniel Meiers <daniel.meiers@cismet.de>
2013-05-07 11:28:35 +0200

    #24 adjusting state of MultipleDownload if one (sub-)Download gets cancelled

--- src/main/java/de/cismet/tools/gui/downloadmanager/DownloadPanel.java
+++ src/main/java/de/cismet/tools/gui/downloadmanager/DownloadPanel.java
@@ -115,6 +115,7 @@ public class DownloadPanel extends javax.swing.JPanel implements Observer {
             add(lblTitle, gridBagConstraints);
 
             gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.gridwidth = 2;
             gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
             gridBagConstraints.anchor = java.awt.GridBagConstraints.LINE_END;
             gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 5);
@@ -179,42 +180,55 @@ public class DownloadPanel extends javax.swing.JPanel implements Observer {
         jxlOpenFile = new org.jdesktop.swingx.JXHyperlink();
         btnCancel = new javax.swing.JButton();
 
-        mniOpenFile.setText(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.mniOpenFile.text")); // NOI18N
+        mniOpenFile.setText(org.openide.util.NbBundle.getMessage(
+                DownloadPanel.class,
+                "DownloadPanel.mniOpenFile.text")); // NOI18N
         mniOpenFile.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniOpenFileActionPerformed(evt);
-            }
-        });
+
+                @Override
+                public void actionPerformed(final java.awt.event.ActionEvent evt) {
+                    mniOpenFileActionPerformed(evt);
+                }
+            });
         popContextMenu.add(mniOpenFile);
 
-        mniOpenDirectory.setText(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.mniOpenDirectory.text")); // NOI18N
+        mniOpenDirectory.setText(org.openide.util.NbBundle.getMessage(
+                DownloadPanel.class,
+                "DownloadPanel.mniOpenDirectory.text")); // NOI18N
         mniOpenDirectory.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniOpenDirectoryActionPerformed(evt);
-            }
-        });
+
+                @Override
+                public void actionPerformed(final java.awt.event.ActionEvent evt) {
+                    mniOpenDirectoryActionPerformed(evt);
+                }
+            });
         popContextMenu.add(mniOpenDirectory);
         popContextMenu.add(sepContextMenuOpenRemove);
 
         mniRemove.setText(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.mniRemove.text")); // NOI18N
         mniRemove.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniRemoveActionPerformed(evt);
-            }
-        });
+
+                @Override
+                public void actionPerformed(final java.awt.event.ActionEvent evt) {
+                    mniRemoveActionPerformed(evt);
+                }
+            });
         popContextMenu.add(mniRemove);
 
         setComponentPopupMenu(popContextMenu);
         setMaximumSize(new java.awt.Dimension(2147483647, 54));
         setMinimumSize(new java.awt.Dimension(135, 54));
         addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                formMouseClicked(evt);
-            }
-        });
+
+                @Override
+                public void mouseClicked(final java.awt.event.MouseEvent evt) {
+                    formMouseClicked(evt);
+                }
+            });
         setLayout(new java.awt.GridBagLayout());
 
-        lblIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/de/cismet/tools/gui/downloadmanager/documenttypes/fallback_single_32.png"))); // NOI18N
+        lblIcon.setIcon(new javax.swing.ImageIcon(
+                getClass().getResource("/de/cismet/tools/gui/downloadmanager/documenttypes/fallback_single_32.png"))); // NOI18N
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridheight = 2;
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
@@ -255,7 +269,9 @@ public class DownloadPanel extends javax.swing.JPanel implements Observer {
         prbProgress.setMaximumSize(new java.awt.Dimension(32767, 15));
         prbProgress.setMinimumSize(new java.awt.Dimension(10, 15));
         prbProgress.setPreferredSize(new java.awt.Dimension(146, 15));
-        prbProgress.setString(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.prbProgress.string")); // NOI18N
+        prbProgress.setString(org.openide.util.NbBundle.getMessage(
+                DownloadPanel.class,
+                "DownloadPanel.prbProgress.string")); // NOI18N
         prbProgress.setStringPainted(true);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
@@ -276,12 +292,14 @@ public class DownloadPanel extends javax.swing.JPanel implements Observer {
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         add(lblTime, gridBagConstraints);
 
-        jxlOpenFile.setText(download.getFileToSaveTo() != null ? download.getFileToSaveTo().getAbsolutePath() : "");
+        jxlOpenFile.setText((download.getFileToSaveTo() != null) ? download.getFileToSaveTo().getAbsolutePath() : "");
         jxlOpenFile.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jxlOpenFileActionPerformed(evt);
-            }
-        });
+
+                @Override
+                public void actionPerformed(final java.awt.event.ActionEvent evt) {
+                    jxlOpenFileActionPerformed(evt);
+                }
+            });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
@@ -291,31 +309,37 @@ public class DownloadPanel extends javax.swing.JPanel implements Observer {
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         add(jxlOpenFile, gridBagConstraints);
 
-        btnCancel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/de/cismet/tools/gui/downloadmanager/res/cross-circle.png_16x16.png"))); // NOI18N
-        btnCancel.setText(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.btnCancel.text")); // NOI18N
-        btnCancel.setToolTipText(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.btnCancel.toolTipText")); // NOI18N
+        btnCancel.setIcon(new javax.swing.ImageIcon(
+                getClass().getResource("/de/cismet/tools/gui/downloadmanager/res/cross-circle.png_16x16.png")));       // NOI18N
+        btnCancel.setText(org.openide.util.NbBundle.getMessage(DownloadPanel.class, "DownloadPanel.btnCancel.text"));  // NOI18N
+        btnCancel.setToolTipText(org.openide.util.NbBundle.getMessage(
+                DownloadPanel.class,
+                "DownloadPanel.btnCancel.toolTipText"));                                                               // NOI18N
         btnCancel.setBorderPainted(false);
         btnCancel.setContentAreaFilled(false);
         btnCancel.setFocusPainted(false);
-        btnCancel.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/de/cismet/tools/gui/downloadmanager/res/cross-circle-frame.png_16x16.png"))); // NOI18N
+        btnCancel.setRolloverIcon(new javax.swing.ImageIcon(
+                getClass().getResource("/de/cismet/tools/gui/downloadmanager/res/cross-circle-frame.png_16x16.png"))); // NOI18N
         btnCancel.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnCancelActionPerformed(evt);
-            }
-        });
+
+                @Override
+                public void actionPerformed(final java.awt.event.ActionEvent evt) {
+                    btnCancelActionPerformed(evt);
+                }
+            });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTH;
         add(btnCancel, gridBagConstraints);
-    }// </editor-fold>//GEN-END:initComponents
+    } // </editor-fold>//GEN-END:initComponents
 
     /**
      * Opens a message dialog if the downloads is erraneous and the user double-clicked the panel.
      *
      * @param  evt  The event object.
      */
-    private void formMouseClicked(final java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseClicked
+    private void formMouseClicked(final java.awt.event.MouseEvent evt) { //GEN-FIRST:event_formMouseClicked
         if (evt.getClickCount() > 1) {
             if (download.getCaughtException() != null) {
                 DownloadManagerDialog.showExceptionDialog(download);
@@ -323,52 +347,52 @@ public class DownloadPanel extends javax.swing.JPanel implements Observer {
                 BrowserLauncher.openURLorFile(download.getFileToSaveTo().getParentFile().getAbsolutePath());
             }
         }
-    }//GEN-LAST:event_formMouseClicked
+    }                                                                    //GEN-LAST:event_formMouseClicked
 
     /**
      * An action listener.
      *
      * @param  evt  The event.
      */
-    private void jxlOpenFileActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jxlOpenFileActionPerformed
+    private void jxlOpenFileActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_jxlOpenFileActionPerformed
         BrowserLauncher.openURLorFile(download.getFileToSaveTo().getAbsolutePath());
-    }//GEN-LAST:event_jxlOpenFileActionPerformed
+    }                                                                               //GEN-LAST:event_jxlOpenFileActionPerformed
 
     /**
      * An action listener.
      *
      * @param  evt  The event.
      */
-    private void mniOpenFileActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniOpenFileActionPerformed
+    private void mniOpenFileActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniOpenFileActionPerformed
         BrowserLauncher.openURLorFile(download.getFileToSaveTo().getAbsolutePath());
-    }//GEN-LAST:event_mniOpenFileActionPerformed
+    }                                                                               //GEN-LAST:event_mniOpenFileActionPerformed
 
     /**
      * An action listener.
      *
      * @param  evt  The event.
      */
-    private void mniOpenDirectoryActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniOpenDirectoryActionPerformed
+    private void mniOpenDirectoryActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniOpenDirectoryActionPerformed
         BrowserLauncher.openURLorFile(download.getFileToSaveTo().getParentFile().getAbsolutePath());
-    }//GEN-LAST:event_mniOpenDirectoryActionPerformed
+    }                                                                                    //GEN-LAST:event_mniOpenDirectoryActionPerformed
 
     /**
      * An action listener.
      *
      * @param  evt  The event.
      */
-    private void mniRemoveActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniRemoveActionPerformed
+    private void mniRemoveActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_mniRemoveActionPerformed
         DownloadManager.instance().removeDownload(download);
-    }//GEN-LAST:event_mniRemoveActionPerformed
+    }                                                                             //GEN-LAST:event_mniRemoveActionPerformed
 
     /**
      * DOCUMENT ME!
      *
      * @param  evt  DOCUMENT ME!
      */
-    private void btnCancelActionPerformed(final java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+    private void btnCancelActionPerformed(final java.awt.event.ActionEvent evt) { //GEN-FIRST:event_btnCancelActionPerformed
         download.cancelDownload();
-    }//GEN-LAST:event_btnCancelActionPerformed
+    }                                                                             //GEN-LAST:event_btnCancelActionPerformed
 
     /**
      * Initiates the icon. According to the file extension of the download, an appropriate icon will be shown.

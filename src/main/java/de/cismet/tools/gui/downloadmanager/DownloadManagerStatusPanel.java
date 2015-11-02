7a628ab2e89550695c2d28206baa4291a7cd8613
JeanAtCismet <jean.ruiz@cismet.de>
2015-11-02 15:06:56 +0100

    fixed #96

--- src/main/java/de/cismet/tools/gui/downloadmanager/DownloadManagerStatusPanel.java
+++ src/main/java/de/cismet/tools/gui/downloadmanager/DownloadManagerStatusPanel.java
@@ -64,7 +64,7 @@ public class DownloadManagerStatusPanel extends javax.swing.JPanel implements Do
                 @Override
                 public void mouseClicked(final MouseEvent me) {
                     if (me.getClickCount() == 2) {
-                        final JDialog downloadManager = DownloadManagerDialog.instance(StaticSwingTools.getParentFrame(DownloadManagerStatusPanel.this));
+                        final JDialog downloadManager = DownloadManagerDialog.getInstance();
                         downloadManager.pack();
                         StaticSwingTools.showDialog(downloadManager);
                     }
@@ -150,7 +150,7 @@ public class DownloadManagerStatusPanel extends javax.swing.JPanel implements Do
             final Download[] downloadArr = downloads.toArray(new Download[downloads.size()]);
             final int tmpComplDownloads = DownloadManager.instance().getCountDownloadsCompleted();
             final int tmpErrDownloads = DownloadManager.instance().getCountDownloadsErroneous();
-            if (!DownloadManagerDialog.instance(StaticSwingTools.getParentFrame(this)).isShowing()) {
+            if (!DownloadManagerDialog.getInstance().isShowing()) {
                 if (completedDownlaods < tmpComplDownloads) {
                     showNotification(downloadArr[0].getTitle(), false);
                 } else if (erroneousDownloads < tmpErrDownloads) {

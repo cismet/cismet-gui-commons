9187a61968418985e3007440634a60168d9f7076
therter <thorsten.herter@cismet.de>
2017-02-15 14:30:36 +0100

    working on #117 jalopy

--- src/main/java/de/cismet/tools/gui/menu/example/ExampleApp.java
+++ src/main/java/de/cismet/tools/gui/menu/example/ExampleApp.java
@@ -1,3 +1,10 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,60 +13,73 @@
 package de.cismet.tools.gui.menu.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import javax.swing.JButton;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
+
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
 /**
+ * DOCUMENT ME!
  *
- * @author therter
+ * @author   therter
+ * @version  $Revision$, $Date$
  */
 public class ExampleApp extends javax.swing.JFrame {
 
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JToolBar jToolBar1;
+    // End of variables declaration//GEN-END:variables
+
+    //~ Constructors -----------------------------------------------------------
+
     /**
-     * Creates new form Test
+     * Creates new form Test.
      */
     public ExampleApp() {
         initComponents();
 
-        Map<String, CidsUiAction> actionMap = new HashMap<String, CidsUiAction>();
+        final Map<String, CidsUiAction> actionMap = new HashMap<String, CidsUiAction>();
 
-        final Collection<? extends CidsUiAction> actions = Lookup.getDefault()
-                .lookupAll(CidsUiAction.class);
+        final Collection<? extends CidsUiAction> actions = Lookup.getDefault().lookupAll(CidsUiAction.class);
 
         final Collection<? extends CidsUiActionProvider> actionProvider = Lookup.getDefault()
-                .lookupAll(CidsUiActionProvider.class);
+                    .lookupAll(CidsUiActionProvider.class);
 
-        for (CidsUiAction action : actions) {
-            actionMap.put((String) action.getValue(CidsUiAction.CIDS_ACTION_KEY), action);
+        for (final CidsUiAction action : actions) {
+            actionMap.put((String)action.getValue(CidsUiAction.CIDS_ACTION_KEY), action);
         }
-        
-        for (CidsUiActionProvider provider : actionProvider) {
-            for (CidsUiAction action : provider.getActions()) {
-                actionMap.put((String) action.getValue(CidsUiAction.CIDS_ACTION_KEY), action);
+
+        for (final CidsUiActionProvider provider : actionProvider) {
+            for (final CidsUiAction action : provider.getActions()) {
+                actionMap.put((String)action.getValue(CidsUiAction.CIDS_ACTION_KEY), action);
             }
         }
 
-        ObjectMapper mapper = new ObjectMapper();
+        final ObjectMapper mapper = new ObjectMapper();
 
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("MenuConfiguration.json")));
-            Menu menu = mapper.readValue(reader, Menu.class);
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        this.getClass().getResourceAsStream("MenuConfiguration.json")));
+            final Menu menu = mapper.readValue(reader, Menu.class);
 
-            Item[] menuItems = menu.getMainMenu();
+            final Item[] menuItems = menu.getMainMenu();
 
-            for (Item tmp : menuItems) {
+            for (final Item tmp : menuItems) {
                 if (tmp.getActionKey() == null) {
-                    JMenu m = new JMenu();
-                    
+                    final JMenu m = new JMenu();
+
                     if (tmp.getName() != null) {
                         m.setText(tmp.getName());
                     } else if (tmp.getI18nKey() != null) {
@@ -69,19 +89,19 @@ public class ExampleApp extends javax.swing.JFrame {
                     addMenuItems(tmp.getItems(), m, actionMap);
                 }
             }
-            
-            Item[] toolbarItems = menu.getMainToolbar();
 
-            for (Item tmp : toolbarItems) {
+            final Item[] toolbarItems = menu.getMainToolbar();
+
+            for (final Item tmp : toolbarItems) {
                 if (tmp.getActionKey() != null) {
-                    CidsUiAction action = actionMap.get(tmp.getActionKey());
+                    final CidsUiAction action = actionMap.get(tmp.getActionKey());
 
                     if (action != null) {
-                        JButton button = jToolBar1.add(action);
+                        final JButton button = jToolBar1.add(action);
 
-                        String name = getActionName(tmp);
+                        final String name = getActionName(tmp);
 
-                        if (name != null ) {
+                        if (name != null) {
                             button.setText(name);
                         }
                     }
@@ -92,63 +112,65 @@ public class ExampleApp extends javax.swing.JFrame {
         }
     }
 
+    //~ Methods ----------------------------------------------------------------
+
     /**
      * Add the given items to the given menu.
-     * 
-     * @param items
-     * @param menu
-     * @param actionMap 
+     *
+     * @param  items      DOCUMENT ME!
+     * @param  menu       DOCUMENT ME!
+     * @param  actionMap  DOCUMENT ME!
      */
-    private void addMenuItems(Item[] items, JMenu menu, Map<String, CidsUiAction> actionMap) {
-        for (Item tmp : items) {
+    private void addMenuItems(final Item[] items, final JMenu menu, final Map<String, CidsUiAction> actionMap) {
+        for (final Item tmp : items) {
             if (tmp.getActionKey() == null) {
-                JMenu m = new JMenu(tmp.getName());
+                final JMenu m = new JMenu(tmp.getName());
                 menu.add(m);
                 addMenuItems(tmp.getItems(), m, actionMap);
             } else {
-                String actionKey = tmp.getActionKey();
-                CidsUiAction action = actionMap.get(actionKey);
+                final String actionKey = tmp.getActionKey();
+                final CidsUiAction action = actionMap.get(actionKey);
 
                 if (action != null) {
-                    JMenuItem menuItem = new JMenuItem(action);
+                    final JMenuItem menuItem = new JMenuItem(action);
                     menu.add(menuItem);
-                    String name = getActionName(tmp);
-                    
-                    if (name != null ) {
+                    final String name = getActionName(tmp);
+
+                    if (name != null) {
                         menuItem.setText(name);
                     }
                 }
             }
         }
     }
-    
+
     /**
      * Determines the name of the given item.
-     * 
-     * @param item
-     * @return the name of the given item
+     *
+     * @param   item  DOCUMENT ME!
+     *
+     * @return  the name of the given item
      */
-    private String getActionName(Item item) {
+    private String getActionName(final Item item) {
         String name = null;
-        
+
         if (item.getName() != null) {
             name = item.getName();
-        }  else if (item.getI18nKey() != null) {
+        } else if (item.getI18nKey() != null) {
             name = NbBundle.getMessage(ExampleApp.class, item.getI18nKey());
         }
-        
+
         return name;
     }
 
     /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
+     * This method is called from within the constructor to initialize the form. WARNING: Do NOT modify this code. The
+     * content of this method is always regenerated by the Form Editor.
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
+        final java.awt.GridBagConstraints gridBagConstraints;
 
         jToolBar1 = new javax.swing.JToolBar();
         jMenuBar1 = new javax.swing.JMenuBar();
@@ -168,49 +190,52 @@ public class ExampleApp extends javax.swing.JFrame {
         setJMenuBar(jMenuBar1);
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    } // </editor-fold>//GEN-END:initComponents
 
     /**
-     * @param args the command line arguments
+     * DOCUMENT ME!
+     *
+     * @param  args  the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(final String[] args) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            for (final javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ExampleApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleApp.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ExampleApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleApp.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ExampleApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleApp.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ExampleApp.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExampleApp.class.getName())
+                    .log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                ExampleApp t = new ExampleApp();
-                
-                t.setSize(300, 300);
-                t.setVisible(true);
-            }
-        });
-    }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JToolBar jToolBar1;
-    // End of variables declaration//GEN-END:variables
+                @Override
+                public void run() {
+                    final ExampleApp t = new ExampleApp();
+
+                    t.setSize(300, 300);
+                    t.setVisible(true);
+                }
+            });
+    }
 }
